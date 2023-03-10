361d4bada40d19016a3752df23b23b58b51a6246
peter-l5 <105649844+peter-l5@users.noreply.github.com>
2023-03-10 12:07:29 +0000

    triangle and circle methods added

--- README.md
+++ README.md
@@ -1,15 +1,27 @@
 # framebuf2
 
-## MicroPython FrameBuffer larger font extension
+## MicroPython FrameBuffer larger font, triangle and circle extension
 
-This module extends MicroPython's [framebuf module](https://docs.micropython.org/en/latest/library/framebuf.html "MicroPython documentation"). It provides double, triple and quadruple size rendering of text using the built-in font provided in the framebuf module. 
-The module has been tested on a Raspberry Pi Pico with a SH1107 display.
+This module extends the `FrameBuffer` class in MicroPython's [framebuf module](https://docs.micropython.org/en/latest/library/framebuf.html "MicroPython documentation"). It enables drawing of double, triple, quadruple, and larger size text to FrameBuffer objects using the built-in font provided in the framebuf module.<br> 
+Also included are methods to draw triangles and circles. These can be outlines only or filled.<br>
+The module has been tested on a Raspberry Pi Pico with a 128x128 pixel SH1107 display. It should work with all MicroPython FrameBuffer objects.
 
 ## Methods
 
-The module adds the large_text() method, `FrameBuffer.large_text(s, x, y, m [, c])` to the FrameBuffer class.
+`large_text(s, x, y, m [, c])` 
 
-Write text, s, to a FrameBuffer using the the x and y coordinates as the upper-left corner of the text. The colour of the text can be defined by the optional argument, c, but is otherwise a default value of 1. The parameter m sets the size multiple for the text. The normal size for text with the 'FrameBuffer.text()' method is 8x8. This would be a multiple of 1. To obtain larger text output, with 16x16 pixel characters, for example, use 2 for the m parameter.
+Write text, `s`, to a FrameBuffer using the the `x` and `y` coordinates as the upper-left corner of the text. The colour of the text can be defined by the optional argument, `c`, but is otherwise a default value of 1. The parameter `m` sets the size multiple for the text. The normal size for text with the 'FrameBuffer.text()' method is 8x8 pixels. This would be a multiple of 1. To obtain larger text output, with 16x16 pixel characters, for example, use 2 for the m parameter.
+
+`circle(x0, y0, radius, c [, f: bool] )` 
+
+Draw a circle centred on `x0, y0` with the specified `radius` and border colour, `c` (integer). Optionally fill the circle by adding `f=True`.
+
+`triangle(x0, y0, x1, y1, x2, y2, c [, f: bool] )` 
+
+Draw a triangle with vertices at points `x0,y0` , `x1,y1` and `x2,y2` and border colour, `c` (integer). Optionally fill the circle by adding `f=True`.
+
+
+## Usage
 
 Example use:
 ```
@@ -17,13 +29,23 @@ Example use:
     display.large_text('double', 0, 0, 2, 1)  # double size text
     display.large_text('size!', 0, 16, 2, 1)
     display.large_text('HUGE', 0, 32, 4, 1)   # quadruple size text
+
+    # draw a circle centred at point (x=64, y=64) with radius 56 and colour 1.
+    display.circle(64, 64, 56 , c=1)
+    display.circle(64, 64, 48 , c=1, f=True) # filled circle
+
+    # draw a filled triangle with corners at (x=0, y=0), (x=0, y=127) and (x=127, y=127)
+    # filled with colour 1.
+    display.triangle(0, 0, 0, 127, 127, 127, c=1, f=True)
 ```
+Additional examples are included in the example code for this [SH1107](https://github.com/peter-l5/SH1107 "SH1107 OLED display driver") display driver
 
 ## Loading the module
 
-Use the following to import the module and extend the FrameBuffer class.  
-`import framebuf2 as framebuf`
+Use the following to import the module and extend the FrameBuffer class.<br>
+`import framebuf2 as framebuf`<br>
+The FrameBuffer class will then offer these additional methods besides all the standard methods.
 
 ## Requirements
 
-Works with MicroPython version 1.18. Will also work with other versions. 
+Works with MicroPython version 1.19.1. Will also work with other versions. 
--- framebuf2.py
+++ framebuf2.py
@@ -1,8 +1,21 @@
 # this code is distributed under the MIT licence.
-# frambuf2 v101
-# (c) 2022-2023 Peter Lumb (peter-l5) 
 
-__version__ = "v101"
+"""
+frambuf2 v202: micropython framebuffer extensions
+(c) 2022-2023 Peter Lumb (peter-l5)
+
+acknowledgement
+the methods: circle() and triangle() are based on the
+adafruit micropython gfx library:
+See repository: https://github.com/adafruit/micropython-adafruit-gfx
+that library was: 
+Port of Adafruit GFX Arduino library to MicroPython
+Based on: https://github.com/adafruit/Adafruit-GFX-Library
+Author: Tony DiCola (original GFX author Phil Burgess)
+License: MIT License (https://opensource.org/licenses/MIT)
+"""
+
+__version__ = "v202"
 __repo__ = "https://github.com/peter-l5/framebuf2"
 
 import framebuf
@@ -17,8 +30,15 @@ GS4_HMSB=framebuf.GS4_HMSB
 GS8=framebuf.GS8
 
 class FrameBuffer(framebuf.FrameBuffer):
-
+    
     def large_text(self, s, x, y, m, c=1):
+        """
+        large text drawing function uses the standard framebuffer font (8x8 pixel characters)
+        writes text, s,
+        to co-cordinates x, y
+        size multiple, m (integer, eg: 1,2,3,4. a value of 2 produces 16x16 pixel characters)
+        colour, c [optional parameter, default value c=1]
+        """
         smallbuffer=bytearray(8)
         letter=framebuf.FrameBuffer(smallbuffer,8,8,framebuf.MONO_HMSB)
         for character in s:
@@ -31,3 +51,132 @@ class FrameBuffer(framebuf.FrameBuffer):
                             for l in range (y+j*m, y+(j+1)*m):
                                 self.pixel(k, l, c)
             x=x+8*m
+
+    def circle(self, x0, y0, radius, c, f: bool = None):
+        """
+        Circle drawing function.  Will draw a single pixel wide circle with
+        center at x0, y0 and the specified radius
+        colour c
+        fill if f is True 
+        """
+        if f is None or f != True:
+            g = 1 - radius
+            ddG_x = 1
+            ddG_y = -2 * radius
+            x = 0
+            y = radius
+            self.pixel(x0, y0 + radius, c)
+            self.pixel(x0, y0 - radius, c)
+            self.pixel(x0 + radius, y0, c)
+            self.pixel(x0 - radius, y0, c)
+            while x < y:
+                if g >= 0:
+                    y -= 1
+                    ddG_y += 2
+                    g += ddG_y
+                x += 1
+                ddG_x += 2
+                g += ddG_x
+                self.pixel(x0 + x, y0 + y, c)
+                self.pixel(x0 - x, y0 + y, c)
+                self.pixel(x0 + x, y0 - y, c)
+                self.pixel(x0 - x, y0 - y, c)
+                self.pixel(x0 + y, y0 + x, c)
+                self.pixel(x0 - y, y0 + x, c)
+                self.pixel(x0 + y, y0 - x, c)
+                self.pixel(x0 - y, y0 - x, c)
+        else:
+            self.vline(x0, y0 - radius, 2*radius + 1, c)
+            g = 1 - radius
+            ddG_x = 1
+            ddG_y = -2 * radius
+            x = 0
+            y = radius
+            while x < y:
+                if g >= 0:
+                    y -= 1
+                    ddG_y += 2
+                    g += ddG_y
+                x += 1
+                ddG_x += 2
+                g += ddG_x
+                self.vline(x0 + x, y0 - y, 2*y + 1, c)
+                self.vline(x0 + y, y0 - x, 2*x + 1, c)
+                self.vline(x0 - x, y0 - y, 2*y + 1, c)
+                self.vline(x0 - y, y0 - x, 2*x + 1, c)
+
+    def triangle(self, x0, y0, x1, y1, x2, y2, c, f: bool = None):
+        """
+        Triangle drawing function.  Will draw a single pixel wide triangle
+        around the points (x0, y0), (x1, y1), and (x2, y2)
+        colour c
+        fill if f is True 
+        """
+        if f is None or f != True:
+            self.line(x0, y0, x1, y1, c)
+            self.line(x1, y1, x2, y2, c)
+            self.line(x2, y2, x0, y0, c)
+        else: 
+            if y0 > y1:
+                y0, y1 = y1, y0
+                x0, x1 = x1, x0
+            if y1 > y2:
+                y2, y1 = y1, y2
+                x2, x1 = x1, x2
+            if y0 > y1:
+                y0, y1 = y1, y0
+                x0, x1 = x1, x0
+            a = 0
+            b = 0
+            y = 0
+            last = 0
+            if y0 == y2:
+                a = x0
+                b = x0
+                if x1 < a:
+                    a = x1
+                elif x1 > b:
+                    b = x1
+                if x2 < a:
+                    a = x2
+                elif x2 > b:
+                    b = x2
+                self.hline(a, y0, b-a+1, c)
+                return
+            dx01 = x1 - x0
+            dy01 = y1 - y0
+            dx02 = x2 - x0
+            dy02 = y2 - y0
+            dx12 = x2 - x1
+            dy12 = y2 - y1
+            if dy01 == 0:
+                dy01 = 1
+            if dy02 == 0:
+                dy02 = 1
+            if dy12 == 0:
+                dy12 = 1
+            sa = 0
+            sb = 0
+            if y1 == y2:
+                last = y1
+            else:
+                last = y1-1
+            for y in range(y0, last+1):
+                a = x0 + sa // dy01
+                b = x0 + sb // dy02
+                sa += dx01
+                sb += dx02
+                if a > b:
+                    a, b = b, a
+                self.hline(a, y, b-a+1, c)
+            sa = dx12 * (y - y1)
+            sb = dx02 * (y - y0)
+            while y <= y2:
+                a = x1 + sa // dy12
+                b = x0 + sb // dy02
+                sa += dx12
+                sb += dx02
+                if a > b:
+                    a, b = b, a
+                self.hline(a, y, b-a+1, c)
+                y += 1
