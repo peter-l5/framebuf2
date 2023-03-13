d76182a9d5d96fed1df680deb0038eb46a5adce7
peter-l5 <105649844+peter-l5@users.noreply.github.com>
2023-03-13 00:59:04 +0000

    large_text method() can now also rotate text
    v206
    - also adds a substantial speed enhancement to the large_text() method especially for larger fonts

--- framebuf2.py
+++ framebuf2.py
@@ -1,7 +1,7 @@
 # this code is distributed under the MIT licence.
 
 """
-frambuf2 v202: micropython framebuffer extensions
+frambuf2 v206: micropython framebuffer extensions
 (c) 2022-2023 Peter Lumb (peter-l5)
 
 acknowledgement
@@ -15,7 +15,7 @@ Author: Tony DiCola (original GFX author Phil Burgess)
 License: MIT License (https://opensource.org/licenses/MIT)
 """
 
-__version__ = "v202"
+__version__ = "v206"
 __repo__ = "https://github.com/peter-l5/framebuf2"
 
 import framebuf
@@ -31,26 +31,50 @@ GS8=framebuf.GS8
 
 class FrameBuffer(framebuf.FrameBuffer):
     
-    def large_text(self, s, x, y, m, c=1):
+    def _reverse(self, s:string) -> string:
+        t=""
+        for i in range(0,len(s)):
+            t += s[len(s)-1-i]
+        return t
+    
+    def large_text(self, s, x, y, m, c:int = 1, r:int = 0, t = None):
         """
         large text drawing function uses the standard framebuffer font (8x8 pixel characters)
         writes text, s,
         to co-cordinates x, y
         size multiple, m (integer, eg: 1,2,3,4. a value of 2 produces 16x16 pixel characters)
         colour, c [optional parameter, default value c=1]
+        optional parameter, r is rotation of the text: 0, 90, 180, or 270 degrees
+        optional parameter, t is rotation of each character within the text: 0, 90, 180, or 270 degrees
         """
+        colour=c
         smallbuffer=bytearray(8)
         letter=framebuf.FrameBuffer(smallbuffer,8,8,framebuf.MONO_HMSB)
+        r = r % 360 // 90
+        dx = 8 * m if r in (0, 2) else 0
+        dy = 8 * m if r in (1, 3) else 0
+        if r in (2, 3):
+            s = self._reverse(s)
+        t = r if t is None else t % 360 // 90
+        a, b, c, d = 1, 0, 0, 1
+        for i in range(0,t):
+            a, b, c, d = c, d, -a, -b
+        x0 = 0 if a + c > 0 else 7
+        y0 = 0 if b + d > 0 else 7
         for character in s:
             letter.fill(0)
             letter.text(character,0,0,1)
             for i in range (0, 8):
                 for j in range (0, 8):
                     if letter.pixel(i, j) == 1:
-                        for k in range (x+i*m, x+(i+1)*m):
-                            for l in range (y+j*m, y+(j+1)*m):
-                                self.pixel(k, l, c)
-            x=x+8*m
+                        p = x0 + a*i + c*j
+                        q = y0 + b*i + d*j
+                        if m == 1:
+                            self.pixel(x+p, y+q, colour)
+                        else:    
+                            self.fill_rect(x+p*m, y+q*m, m, m, colour)
+            x += dx
+            y += dy
 
     def circle(self, x0, y0, radius, c, f: bool = None):
         """
@@ -104,7 +128,7 @@ class FrameBuffer(framebuf.FrameBuffer):
                 self.vline(x0 + y, y0 - x, 2*x + 1, c)
                 self.vline(x0 - x, y0 - y, 2*y + 1, c)
                 self.vline(x0 - y, y0 - x, 2*x + 1, c)
-
+                
     def triangle(self, x0, y0, x1, y1, x2, y2, c, f: bool = None):
         """
         Triangle drawing function.  Will draw a single pixel wide triangle
